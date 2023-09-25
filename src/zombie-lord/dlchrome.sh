787365e0e586a508b087ca48086b867908c2f773
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2023-09-25 13:39:48 +0800

    Edge dl

--- src/zombie-lord/dlchrome.sh
+++ src/zombie-lord/dlchrome.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 
+# Determine the system architecture
 amd64=$(dpkg --print-architecture || uname -m)
-rm google-chrome-stable_current_$amd64.deb || :
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_$amd64.deb
-if [[ -f google-chrome-stable_current_$amd64.deb ]]; then
-  sudo dpkg -i google-chrome-stable_current_$amd64.deb 
+
+# Remove any existing downloaded files
+rm microsoft-edge-stable_current_$amd64.deb || :
+
+# Download the Microsoft Edge .deb package
+wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_current_$amd64.deb
+
+# Check if the file was downloaded successfully
+if [[ -f microsoft-edge-stable_current_$amd64.deb ]]; then
+  # Install the downloaded package
+  sudo dpkg -i microsoft-edge-stable_current_$amd64.deb 
+
+  # Fix any broken dependencies
   sudo apt --fix-broken -y install
-  rm google-chrome-stable_current_$amd64.deb || :
-  #sudo apt -y install google-chrome-stable
-  #sudo apt --fix-broken install
+
+  # Remove the downloaded package
+  rm microsoft-edge-stable_current_$amd64.deb || :
 else
+  # Fallback to installing Chromium if the download fails
   sudo apt install -y chromium
 fi
 
